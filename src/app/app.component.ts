8a451b8824b2ccbc6cca90bce1636723c02160dd
Xiomara <Xiomara Pulido Balmaceda>
2018-04-03 09:26:55 -0500

    This project contains some examples of angular pipes

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  nombre = 'Xiomara';
+
+  arreglo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  PI = Math.PI;
+
+  a = 0.234;
+
+}
